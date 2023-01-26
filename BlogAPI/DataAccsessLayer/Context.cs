8b89e4c0cf5303349fcf463188c3c9733ee69567
ErenUnver <unver_eren@hotmail.com>
2023-01-26 17:18:17 +0300

    CRUD işlemleri için Ajax kullanıldı.API Projesi eklendi.Identity kütüphanesi kullanıldı.

---
+++ BlogAPI/DataAccsessLayer/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogAPI.DataAccsessLayer
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=305-08;Database=Db_BlogCoreeAPI;User=WebMobile_302;Password=WebMobile.302;");
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ DataAccessLayer/Migrations/20230126140508_m4.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class m4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+        }
+    }
+}
